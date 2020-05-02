fabd06b53092525d4a4d61ef060a5de071886118
Bala Luguja <bluguja@gmail.com>
2020-05-02 00:36:05 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -21,7 +21,7 @@ def appointments
 end
 
 
-  def new_appointment(date, patient)
+  def new_appointment(patient, date)
      appointment = Appointment.new(patient, date, self)
      @appointments << appointment
      appointment
