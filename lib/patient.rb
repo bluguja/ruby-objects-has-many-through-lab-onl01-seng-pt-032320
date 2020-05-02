f21949e1c0e77b8975b63019724d66dd54c190dc
Bala Luguja <bluguja@gmail.com>
2020-05-02 00:31:36 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,8 +1,15 @@
 class Patient
   attr_accessor :name , :appointments
   
+  @@all = []
+  
   def initialize(name)
     @name=name
+    @@all<< self
+  end
+  
+  def self.all 
+    @@all
   end
   
    def new_appointment(doctor, date)
